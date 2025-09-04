77afa41c1cc5cff98f3e322c9ab5bdce9cad5507
alexttz <lima.alexander.las@gmail.com>
2025-09-04 20:20:30 -0300

    Aula 03 VueJS - Criando o arquivo TheHeader, adicionando o componente na aplicação App.vue e estilizando o elemento

---
+++ app/src/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+    <header class="header">
+        <h1>This is Header</h1>
+    </header>
+</template>
+
+<script>
+    export default{
+
+        }
+
+</script>
+
+<style>
+
+    .header {
+        background-color: #42b983;
+        padding: 20px;
+        color: white;
+        text-align: center;
+    }
+
+</style>
